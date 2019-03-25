ea8506e96fadcf09ab5b080a9ea388d79f2666aa
david@sootball <david.robinson-9@postgrad.manchester.ac.uk>
2019-03-25 07:09:36 +0000

    Generalized run_code.sh
    Notes
    * Made the script more general in run_code.sh

--- run_code.sh
+++ run_code.sh
@@ -10,13 +10,16 @@ else
   driver_name=$1
 fi
 
-initial_area=0.033 #0.032592
-arguments="--case 2"
+# rectangle initial_area 0.033 and circle clamped/resting 0.2
+initial_area=0.033 # 0.2 #0.033 #0.05 #0.1 #33 #0.032592
+arguments="--case 2" #"--case 2"
+# rectangle base 0.65049 and circle clamped resting 0.5 
+base=0.65049 #0.5 #0.65049 #$(echo "scale =10 ; 1/sqrt(2)" | bc) #0.70711" #"0.5" #0.65049 
 
 # Loop over 7 plot points
 for it in `seq 0 6`;
 do
-elementarea=$(echo "scale =10 ; $initial_area * (0.65049)^($it)" | bc)
+elementarea=$(echo "scale =10 ; $initial_area * ($base)^($it)" | bc)
 echo "##########################################"
 echo Running:
 echo "./$driver_name  $arguments --element_area $elementarea >> output"
